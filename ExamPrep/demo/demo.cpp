d9cda7a7cdebc8b178ff2872a175ab93863f4c69
DiyanKalaydzhiev23 <diankostadenov@gmail.com>
2023-10-14 12:14:45 +0300

    Added nore solutions

--- ExamPrep/demo/demo.cpp
+++ ExamPrep/demo/demo.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    std::cout << "Hello World!\n";
+    int arr[5] = { 11, 2, 33, -4, 51 };
+
+    for (int i = 0; i <= 5; i++) {
+        cout << arr[i] << endl;
+    }
 }
